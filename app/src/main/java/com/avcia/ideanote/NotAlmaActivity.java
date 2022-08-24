c66a8d9b3fa8a54253e106d78604e95f91ea18b5
Horatius23 <110195852+Horatius23@users.noreply.github.com>
2022-08-24 13:28:02 +0300

    new additions: -fixed Depreceated API's -Finished Code part -Started Aesthetics Phase

--- app/src/main/java/com/avcia/ideanote/NotAlmaActivity.java
+++ app/src/main/java/com/avcia/ideanote/NotAlmaActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.view.*;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -29,31 +29,28 @@ public class NotAlmaActivity extends AppCompatActivity {
         imageView_save = findViewById(R.id.imageView_save);
         editText_baslik = findViewById(R.id.editText_baslik);
         editText_notlar = findViewById(R.id.editText_notlar);
-        imageView_save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String baslik = editText_baslik.getText().toString();
-                String betimleme = editText_notlar.getText().toString();
-                if (betimleme.isEmpty()){
-                    Toast.makeText(NotAlmaActivity.this, "Lütfen Bir Not Ekleyin", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                String pattern = "EEE MMM yyyy HH:mm:ss";
-                SimpleDateFormat simpleTarih =new SimpleDateFormat(pattern,
-                        new Locale("tr", "TR"));
-                String date = simpleTarih.format(new Date());
-                System.out.println(date);
-
-                notlar = new Notlar();
-                notlar.setNotlar(betimleme);
-                notlar.setTarih(simpleTarih.format(date));
-
-                Intent intent = new Intent();
-                intent.putExtra("Not", notlar);
-                setResult(Activity.RESULT_OK, intent);
-                finish();
+        //lamdaya çevrildi
+        imageView_save.setOnClickListener(v -> {
+            String baslik = editText_baslik.getText().toString();
+            String betimleme = editText_notlar.getText().toString();
+            if (betimleme.isEmpty()){
+                Toast.makeText(NotAlmaActivity.this, "Lütfen Bir Not Ekleyin", Toast.LENGTH_SHORT).show();
+                return;
             }
+            String pattern = "EEE MMM yyyy HH:mm:ss";
+            SimpleDateFormat simpleTarih =new SimpleDateFormat(pattern,
+                    new Locale("tr", "TR"));
+            String date = simpleTarih.format(new Date());
+            System.out.println(date);
+
+            notlar = new Notlar();
+            notlar.setNotlar(betimleme);
+            notlar.setTarih(simpleTarih.format(date));
+
+            Intent intent = new Intent();
+            intent.putExtra("Not", notlar);
+            setResult(Activity.RESULT_OK, intent);
+            finish();
         });
     }
 }
